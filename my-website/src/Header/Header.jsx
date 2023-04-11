0aa005231a81778dc57cfad0198ae9284b1067b1
Kyle Martinelli <kylelmartinelli@gmail.com>
2023-04-10 20:15:30 -0700

    setup commit

---
+++ my-website/src/Header/Header.jsx
@@ -0,0 +1,16 @@
+import "./HeaderStyles.css";
+
+function Header() {
+  return (
+    <div className="header-main">
+      <div className="header-buttons">
+        <button>About</button>
+        <button>Work</button>
+        <button>Contact</button>
+        <button>Resume</button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
